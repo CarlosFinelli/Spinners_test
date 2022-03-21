f426b827387fe0c2fbbe5a8190c509b4155a4c63
Carlos Guilherme Miranda Finelli <carlosmfinelli@gmail.com>
2022-03-21 15:24:52 -0300

    Initial commit

---
+++ app/src/main/java/com/example/spinnerstest/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.spinnerstest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        val autoComplete = findViewById<AutoCompleteTextView>(R.id.autoComplete)
+
+        val type = arrayOf ("Bed-sitter", "Single", "1 - Bedroom", "2 - Bedroom", "3 - Bedroom")
+        val adapter = ArrayAdapter(this, R.layout.drop_down_item, type)
+        
+        autoComplete.setAdapter(adapter)
+        spinner.adapter = adapter
+
+        autoComplete.setOnItemClickListener { parent, view, position, id ->
+            Toast.makeText(this, autoComplete.text.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+}
