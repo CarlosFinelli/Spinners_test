a6b970495542f3351df6fae1f19edb8761844597
Carlos Guilherme Miranda Finelli <carlosmfinelli@gmail.com>
2022-03-21 15:35:15 -0300

    Alteração para exibir os três distintos tipos de layout dos spinners na tela

--- app/src/main/java/com/example/spinnerstest/MainActivity.kt
+++ app/src/main/java/com/example/spinnerstest/MainActivity.kt
@@ -12,14 +12,20 @@ class MainActivity : AppCompatActivity() {
 
         val spinner = findViewById<Spinner>(R.id.spinner)
         val autoComplete = findViewById<AutoCompleteTextView>(R.id.autoComplete)
+        val autoCompleteFilled = findViewById<AutoCompleteTextView>(R.id.autoComplete_filled)
 
         val type = arrayOf ("Bed-sitter", "Single", "1 - Bedroom", "2 - Bedroom", "3 - Bedroom")
         val adapter = ArrayAdapter(this, R.layout.drop_down_item, type)
-        
+
         autoComplete.setAdapter(adapter)
+        autoCompleteFilled.setAdapter(adapter)
         spinner.adapter = adapter
 
         autoComplete.setOnItemClickListener { parent, view, position, id ->
+            Toast.makeText(this, autoComplete.text.toString(), Toast.LENGTH_SHORT).show()
+        }
+
+        autoCompleteFilled.setOnItemClickListener { parent, view, position, id ->
             Toast.makeText(this, autoComplete.text.toString(), Toast.LENGTH_SHORT).show()
         }
     }
